f26ee163c6932d6863b087a230db973a8a749bff
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2023-09-15 18:36:55 +0700

    header new, footer new

---
+++ home/main.js
@@ -0,0 +1,27 @@
+
+document.onscroll = function () {
+    const scroll = window.scrollY;
+    closeNavBar = document.querySelector(".header-navbar-close");
+    navBar = document.querySelector(".header-navbar");
+    if (scroll > 87) {
+      document.querySelector("header").style.background = "white";
+      document.querySelector("header").style.boxShadow =
+        " 0 3px 3px #ced4da";
+    } else {
+      document.querySelector("header").style.background = "transparent";
+      document.querySelector("header").style.boxShadow = " none";
+    }
+  
+    if (scroll < 300) {
+      document.querySelector(".bottom-btn").style.display = "none";
+    } else {
+      document.querySelector(".bottom-btn").style.display = "block";
+    }
+  };
+  
+  let burger = document.querySelector(".burger-box");
+  let body = document.querySelector("body");
+  
+  burger.onclick = function () {
+    body.classList.toggle("active");
+  };
