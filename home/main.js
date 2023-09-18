9b713041aca0710c3a78d3d3e979462c97ba9710
DangPhuongTay <89719935+DangPhuongTay@users.noreply.github.com>
2023-09-19 00:58:10 +0700

    profile, change, detail-product

--- home/main.js
+++ home/main.js
@@ -3,12 +3,7 @@ document.onscroll = function () {
     const scroll = window.scrollY;
     closeNavBar = document.querySelector(".header-navbar-close");
     navBar = document.querySelector(".header-navbar");
-    if (scroll > 87) {
-      document.querySelector("header").style.background = "white";
-    } else {
-      document.querySelector("header").style.background = "transparent";
-      document.querySelector("header").style.boxShadow = " none";
-    }
+
   
     if (scroll < 300) {
       document.querySelector(".bottom-btn").style.display = "none";
